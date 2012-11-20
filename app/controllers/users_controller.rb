d45cddfe977ca128ab2dce6b71bedff81de8bf30
Kevin Smouts <kevin.smouts@gmail.com>
2012-11-19 21:43:42 -0800

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_url, :notice => "Successfully created user."
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to root_url, :notice  => "Successfully updated user."
+    else
+      render :action => 'edit'
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParisomaJobBoard::Application.config.secret_token = '301f8816e0ec6b6336bbbf78a6d9aff51ba52d71482385ada12bf4984d25877231d418cf2089dd3ae1ec3802ede62fc8be527770727fa856b2d97cd82008a105'
